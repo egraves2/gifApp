a2154abdd954db983cd7b366e0271f2cb34fccf0
Erin Graves <egraveshna16@gmail.com>
2020-02-05 16:22:31 -0800

    loading bar and open in new tab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/Textfield'
+import Button from '@material-ui/core/Button'
+import { Search } from '@material-ui/icons'
+import { LinearProgress } from '@material-ui/core'
 
 function App() {
+  const [text, setText] = useState('')
+  const [memes, setMemes] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  async function getMemes(){
+    setLoading(true)
+    setMemes([])
+    const key = 'O7X4eO7OX8mzODs1qClF1QNuuEK6EWIa'
+    let url = 'https://api.giphy.com/v1/gifs/search?'
+    url += 'api_key=' + key
+    url += '&q=' + text
+    const r = await fetch(url)
+    const body = await r.json()
+    setMemes(body.data)
+    setText('')
+    setLoading(false)
+  }
+
+  console.log(memes)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input-wrap">
+          <TextField fullWidth  variant="outlined" 
+            label="Search for memes"
+            value={text}
+            onChange={e=> setText(e.target.value)}
+            onKeyPress={e=>{
+            if(e.key==='Enter')
+                getMemes()
+            }}
+          />
+          <Button variant="contained" color="primary"
+            onClick={getMemes}
+          >
+            <Search />
+          </Button>
+        </div>
+      
       </header>
+
+      {loading && <LinearProgress />}
+
+      <div className="memes">
+        {memes.map((meme,i)=> <Meme key={i} {...meme}/>)}
+      </div>
+
     </div>
   );
+}
+
+function Meme({images, title}){
+  const url = images.fixed_height.url
+  return (<div className="meme" onClick={()=>window.open(url, '_blank')}>
+    <div className="meme-title">{title}</div>
+    <img height="200" alt="meme" src={url}/>
+  </div>)
 }
 
 export default App;
